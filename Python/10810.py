670661fc7bc7534b104e913418ee0703737a3fd0
sunz8281 <sunz8281@gmail.com>
2024-09-11 08:29:23 +0900

    add 10810

---
+++ Python/10810.py
@@ -0,0 +1,10 @@
+n, m= map(int, input().split())
+arr = [0]*(n)
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    for i in range(a-1, b):
+        arr[i] = c
+
+for i in range(n):
+    print(arr[i], end=' ')
