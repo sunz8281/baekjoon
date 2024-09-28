e6838764608ee37f7df0b591922eb273e45dfded
sunz8281 <sunz8281@gmail.com>
2024-09-28 21:56:16 +0900

    refactor 1052

--- Python/1052.py
+++ Python/1052.py
@@ -1,9 +1,9 @@
 n, k = map(int, input().split())
 
-cnt=0
-while list(str(bin(n))).count('1') > k:
-    x = 1 << list(str(bin(n)))[::-1].index('1')
+cnt = 0
+while bin(n).count('1') > k:
+    x = 1 << bin(n)[::-1].index('1')
     n += x
-    cnt+=x
+    cnt += x
 
 print(cnt)
