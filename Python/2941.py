993103cdf8d4862f0b490684a78f7f6111b41e54
sunz8281 <sunz8281@gmail.com>
2024-09-30 00:44:41 +0900

    refactor 2941

--- Python/2941.py
+++ Python/2941.py
@@ -3,8 +3,6 @@ d = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 s = input()
 cnt = 0
 for i in range(8):
-    cnt+=s.count(d[i])
     s = s.replace(d[i], ' ')
 
-s = s.replace(' ', '')
-print(cnt+len(s))
+print(len(s))
