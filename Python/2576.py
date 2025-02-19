8d7409ce917fa4687d4667e8ee3226067ee3e13d
sunz8281 <sunz8281@gmail.com>
2025-02-19 17:44:07 +0900

    fix 2576

--- Python/2576.py
+++ Python/2576.py
@@ -2,5 +2,8 @@ lst = []
 for _ in range(7):
     n = int(input())
     if n % 2: lst.append(n)
-print(sum(lst))
-print(min(lst))
+if lst:
+    print(sum(lst))
+    print(min(lst))
+else:
+    print(-1)
