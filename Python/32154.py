e3e6fcecccca6c0730840519d429908d3978395b
sunz8281 <sunz8281@gmail.com>
2025-06-27 23:34:43 +0900

    add 32154

---
+++ Python/32154.py
@@ -0,0 +1,4 @@
+a = ['A B C D E F G H J L M', 'A C E F G H I L M', 'A C E F G H I L M', 'A B C E F G H L M', 'A C E F G H L M', 'A C E F G H L M', 'A C E F G H L M', 'A C E F G H L M', 'A C E F G H L M', 'A B C F G H L M']
+n = int(input())-1
+print(len(a[n].split()))
+print(a[n])
