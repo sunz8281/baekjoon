92fd7c59dd01c29612df0c56d17f9b555b76043e
sunz8281 <sunz8281@gmail.com>
2025-12-06 21:29:15 +0900

    add 28249

---
+++ Python/28249.py
@@ -0,0 +1,13 @@
+SHU = {
+    "Poblano":1500,
+    "Mirasol":6000,
+    "Serrano":15500,
+    "Cayenne":40000,
+    "Thai":75000,
+    "Habanero":125000
+}
+n = int(input())
+result = 0
+for _ in range(n):
+    result += SHU[input()]
+print(result)
