8b02927b3b3c48b7e7cab48197dd4dd1451321d1
sunz8281 <sunz8281@gmail.com>
2025-12-21 23:48:52 +0900

    refactor 16236

--- Python/16236.py
+++ Python/16236.py
@@ -1,6 +1,7 @@
 from collections import deque
 
 dx, dy = [0, 0, -1, 1], [-1, 1, 0, 0]
+INF = float('inf')
 
 n = int(input())
 lst = []
@@ -17,20 +18,24 @@ turn = 0
 eat_count = 0
 while True:
     space = [[-1] * n for _ in range(n)]
-    q = deque([(0, s_x, s_y)])
-    fish = (1000000, )
+    space[s_x][s_y] = 0
+    q = deque([(s_x, s_y)])
+    fish = (INF, INF, INF)
     while q:
-        cost, x, y = q.popleft()
-        if space[x][y] != -1 and space[x][y] <= cost: continue
+        x, y = q.popleft()
+        cost = space[x][y]
         if 0 != lst[x][y] < shark: fish = min(fish, (cost, x, y))
-        space[x][y] = cost
         if cost >= fish[0]: continue
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            if nx < 0 or nx >= n or ny < 0 or ny >= n or lst[x][y] > shark: continue
-            q.append((cost+1, nx, ny))
+        for d in range(4):
+            nx, ny = x + dx[d], y + dy[d]
+            if (nx < 0 or nx >= n or ny < 0 or ny >= n
+                    or lst[nx][ny] > shark
+                    or space[nx][ny] != -1):
+                continue
+            q.append((nx, ny))
+            space[nx][ny] = cost+1
 
-    if fish == (1000000, ):
+    if fish[0] >= INF:
         break
     fish_cost, fish_x, fish_y = fish
     turn += fish_cost
