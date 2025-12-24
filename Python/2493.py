bd6a31403f8d0f5285522f830d228ab0066cf888
sunz8281 <sunz8281@gmail.com>
2025-12-24 10:57:23 +0900

    add 2493

---
+++ Python/2493.py
@@ -0,0 +1,15 @@
+n = int(input())
+tower = list(map(int, input().split()))
+
+stack = []
+for i in range(n):
+    result = -1
+    while stack:
+        if tower[stack[-1]] < tower[i]:
+            stack.pop()
+        else:
+            result = stack.pop()
+            stack.append(result)
+            break
+    print(result+1, end=' ')
+    stack.append(i)
