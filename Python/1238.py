4c90546520615869077f6c4f795ed2feada7d884
sunz8281 <sunz8281@gmail.com>
2026-01-05 19:43:51 +0900

    refactor 1238

--- Python/1238.py
+++ Python/1238.py
@@ -2,27 +2,28 @@ import heapq
 
 n, m, x = map(int, input().split())
 road = [[] for _ in range(n)]
+rev_road = [[] for _ in range(n)]
 for _ in range(m):
     a, b, t = map(int, input().split())
     road[a-1].append((t, b-1))
+    rev_road[b-1].append((t, a-1))
 
-def bfs(start, end):
+INF = float('inf')
+def dijkstra(start, graph):
     q = [(0, start)]
     dist = [INF] * n
     dist[start] = 0
     while q:
         cost, destination = heapq.heappop(q)
         if dist[destination] < cost: continue
-        dist[destination] = cost
-        if destination == end: break
-        for new in road[destination]:
-            new_cost, new_dest = new
+        for new_cost, new_dest in graph[destination]:
             if cost + new_cost < dist[new_dest]:
-                heapq.heappush(q, (cost + new_cost, new_dest))
-    return dist[end]
+                new_cost = cost + new_cost
+                dist[new_dest] = new_cost
+                heapq.heappush(q, (new_cost, new_dest))
+    return dist
 
-INF = float('inf')
-result = [0] * n
-for i in range(n):
-    result[i] = bfs(i, x-1) + bfs(x-1, i)
-print(max(result))
+go = dijkstra(x-1, road)
+back = dijkstra(x-1, rev_road)
+hap = [go[i] + back[i] for i in range(n)]
+print(max(hap))
