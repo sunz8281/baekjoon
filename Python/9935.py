a0c171ff1c2ef4fa8ba4d2d8881883efe91d0e19
sunz8281 <sunz8281@gmail.com>
2026-01-08 14:10:53 +0900

    add 9935

--- Python/9935.py
+++ Python/9935.py
@@ -1,13 +1,13 @@
 s = input()
-bomb = input()
+bomb = list(input())
 len_bomb = len(bomb)
 
 stack = []
 for i in range(len(s)):
     stack.append(s[i])
-    while ''.join(stack[-len_bomb:]) == bomb:
-        for _ in range(len_bomb):
-            del stack[-1]
+    while stack[-len_bomb:] == bomb:
+        del stack[-len_bomb:]
+
 
 if stack:
     print(''.join(stack))
