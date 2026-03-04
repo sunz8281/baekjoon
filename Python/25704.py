b360ccfa6d4568c1e445d15067a410203510e916
sunz8281 <sunz8281@gmail.com>
2026-03-05 00:25:15 +0900

    fix 25704

--- Python/25704.py
+++ Python/25704.py
@@ -1,6 +1,8 @@
 n = int(input())
 p = int(input())
-if n>=15: print(min(p*75//100, p-2000))
-elif n>=10: print(min(p*90//100, p-500))
-elif n>=5: print(p-500)
-else: print(p)
+if n>=20: x = min(p*75//100, p-2000)
+elif n>=15: x = min(p*90//100, p-2000)
+elif n>=10: x = min(p*90//100, p-500)
+elif n>=5: x = p-500
+else: x = p
+print(max(0, x))
